02763b7d093fe6baab3e1f0c5ccc03a54c6559c6
Anatoli Rak <Rack.anatoly@gmail.com>
2023-01-23 23:23:59 +0100

    First progect React Native

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [value, setValue] = useState('')
+    return (
+        <View style={styles.container}>
+            <Text style={[globalStyles.text, styles.text]}>Багов нет</Text>
+            {/* <TextInput style={[globalStyles.border, styles.input]} value={value} onChangeText={setValue}/> */}
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'center',
+       
+    },
+    text: {
+        width: 200,
+        fontSize: 18,
+        padding: 8 
+    }
 });
+
+const globalStyles = StyleSheet.create({
+    border: {
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "red"
+    }
+})
